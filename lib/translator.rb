783f8c93215f94356707201fcfcf74d9ab9fc822
Darian Garcia <Garcia.Darian.Lee@gmail.com>
2018-05-09 10:55:56 -0600

    first commit

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,10 @@
+require 'pry'
+
 class Translator
 
   def initialize
+    @letter_count = 0
+    @morse_count = 0
     @dictionary = {"a" => ".-",
                     "b" => "-...",
                     "c" => "-.-.",
@@ -39,4 +43,47 @@ class Translator
                     "0" => "-----",
                     " " => " "}
   end
+
+  def eng_to_morse(word) # passing in a string
+    total_word = ""
+    word_over = false
+    while word_over == false
+      @dictionary.each do |letter, morse|
+        if word[@letter_count].to_s.downcase == letter
+          total_word += morse
+          @letter_count += 1
+        end
+        if @letter_count == word.length
+          word_over = true
+        end
+      end
+    end
+    @letter_count = 0
+    return total_word
+  end
+
+  def morse_to_eng(code)
+    total_word = ""
+    morse_over = false
+    while morse_over == false
+      @dictionary.each do |letter, morse|
+        temp = code[@morse_count..code.index(' ')]
+        puts "LOOK AT ME!"
+        puts temp
+        if temp == morse
+          total_word += letter
+        end
+        @morse_count = code.index(' ') + 1
+        if @morse_count == code.length
+          morse_over = true
+        end
+      end
+    end
+    return total_word
+  end
+
+
+
+
+
 end
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,30 @@
-# YOUR TESTS GO IN HERE!
+require './lib/translator'
+require 'minitest/autorun'
+
+class TranslatorTest < MiniTest::Test
+
+  def test_lower_case
+    translator = Translator.new
+    answer = "......-...-..--- .-----.-..-..-.."
+    temp = translator.eng_to_morse("hello world").to_s
+    assert_equal true, temp == answer
+  end
+
+  def test_with_numbers_ignore_case
+    translator = Translator.new
+    temp = translator.eng_to_morse("There are 3 ships").to_s
+    answer = "-......-.. .-.-.. ...-- ..........--...."
+    assert_equal true, temp == answer
+  end
+
+  def test_morse_to_eng
+    translator = Translator.new
+    temp = translator.morse_to_eng(".... . .-.. .-.. ---  .-- --- .-. .-.. -..")
+    puts temp
+    answer = "hello world"
+    puts answer
+    assert_equal true, temp == answer
+  end
+
+
+end
